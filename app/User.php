024f58ca17ae7e9e7547bca78812363a23d7e9fa
Semih ERDOGAN <hasansemiherdogan@gmail.com>
2018-11-21 21:24:04 +0300

    ADD: generate token method

--- app/User.php
+++ app/User.php
@@ -27,4 +27,22 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Generates Api Token for API
+     * @return bool
+     */
+    public function generateApiToken()
+    {
+        $tokenExists = true;
+        while ($tokenExists) {
+            $this->api_token = str_random(60);
+            $tokenExists = $this
+                ->where('api_token', $this->api_token)
+                ->exists();
+        }
+
+        $this->api_token_expires_at = \Carbon\Carbon::now()->addHour(4);
+        return $this->save();
+    }
 }
